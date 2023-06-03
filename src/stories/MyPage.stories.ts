df91caed5041c3afe8003985dca5de081af43e5a
高橋舞 <tkjnpamw5611@gmail.com>
2023-06-03 16:06:24 +0900

    MyPage.stories.ts でのみ、Viewport の設定をメタデータ/各ストーリーに定義

--- src/stories/MyPage.stories.ts
+++ src/stories/MyPage.stories.ts
@@ -1,52 +1,51 @@
 import MyPage from "../components/MyPage.vue";
 import type { Meta, StoryObj } from "@storybook/vue3";
-import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 type Story = StoryObj<typeof MyPage>;
 
 const meta: Meta<typeof MyPage> = {
-    title: "MyPage",
-    component: MyPage,
-    render: () => ({
-      components: { MyPage },
-      template: "<MyPage />",
-    }),
-    // PC, Mobile それぞれのビューポートを作成 (高さは指定しない)
-    parameters: {
-      backgrounds: {
-        default: "twitter",
-        values: [
-          {
-            name: "twitter",
-            value: "#00aced",
+  title: "MyPage",
+  component: MyPage,
+  render: () => ({
+    components: { MyPage },
+    template: "<MyPage />",
+  }),
+  parameters: {
+    viewport: {
+      viewports: {
+        pc: {
+          name: "Min PC Layout",
+          styles: {
+            width: "992px",
+            height: "100%",
           },
-          {
-            name: "facebook",
-            value: "#3b5998",
+        },
+        mobile: {
+          name: "Min Mobile Layout",
+          styles: {
+            width: "375px",
+            height: "100%",
           },
-        ],
+        },
       },
     },
-  };
-
-// ここで指定している defaultViewport は、前回定義したカスタム Viewport です
+  },
+};
 
 export const ForPc: Story = {
-    parameters: {
-      viewport: {
-        defaultViewport: "pc",
-      },
+  parameters: {
+    viewport: {
+      defaultViewport: "pc",
     },
-  };
-  
-  export const ForMobile: Story = {
-    parameters: {
-      viewport: {
-        defaultViewport: "mobile",
-      },
+  },
+};
+
+export const ForMobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile",
     },
-  };
-  
-export const Default: Story = {};
+  },
+};
 
 export default meta;
