8b0009bd3a54cc0c2d8d288dd57640ab4550859c
高橋舞 <tkjnpamw5611@gmail.com>
2023-06-03 15:06:54 +0900

    Viewport のプリセットを変更する

--- src/stories/MyPage.stories.ts
+++ src/stories/MyPage.stories.ts
@@ -1,16 +1,23 @@
 import MyPage from "../components/MyPage.vue";
 import type { Meta, StoryObj } from "@storybook/vue3";
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 
 type Story = StoryObj<typeof MyPage>;
 
 const meta: Meta<typeof MyPage> = {
-  title: "MyPage",
-  component: MyPage,
-  render: () => ({
-    components: { MyPage },
-    template: "<MyPage />",
-  }),
-};
+    title: "MyPage",
+    component: MyPage,
+    render: () => ({
+      components: { MyPage },
+      template: "<MyPage />",
+    }),
+    // parameters で Viewport のプリセットを設定
+    parameters: {
+      viewport: {
+        viewports: INITIAL_VIEWPORTS,
+      },
+    },
+  };
 
 export const Default: Story = {};
 
