ecfda641a2600df94a8b559548270c12982436f1
高橋舞 <tkjnpamw5611@gmail.com>
2023-06-03 15:55:02 +0900

    .mdx を追加。Storybook に .mdx ファイルを読み込ませる

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   framework: "@storybook/vue3-vite",
-  stories: ["../src/**/*.stories.@(js|ts)"],
+  stories: ["../src/**/*.stories.@(js|ts)", "../src/**/*.mdx"],
   addons: [
     "@storybook/addon-controls",
     "@storybook/addon-actions",
