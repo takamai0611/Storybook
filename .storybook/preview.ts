a9c1d0af11c9732dfb376f77662bd58a81aca448
高橋舞 <tkjnpamw5611@gmail.com>
2023-06-03 16:16:27 +0900

    ラッパーコンポーネントを作る,ストーリーの周囲にマージンを設けるデコレータを定義

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,27 +1,10 @@
-/**
- * Storybook 全体での parameters のデフォルト値を定義する
- */
-export const parameters = {
-    // Viewport addon で使用される設定
-    viewport: {
-      // プリセットをアプリケーションの仕様に合わせて定義
-      viewports: {
-        pc: {
-          name: "Min PC Layout",
-          styles: {
-            width: "992px",
-            height: "100%",
-          },
-        },
-        mobile: {
-          name: "Min Mobile Layout",
-          styles: {
-            width: "375px",
-            height: "100%",
-          },
-        },
-      },
-      // すべてのストーリーでデフォルト PC ビューを使用する
-      defaultViewport: "pc",
-    },
-  };
+import { Decorator } from "@storybook/vue3";
+
+export const decorators: Decorator[] = [
+  () => {
+    return {
+      template:
+        '<div style="margin: 5em; border: 1px solid; border-color: white"><story /></div>',
+    };
+  },
+];
