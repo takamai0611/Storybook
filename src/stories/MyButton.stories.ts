bf54201132566b33cbfda68f5c06f619495f2a70
高橋舞 <tkjnpamw5611@gmail.com>
2023-06-03 14:02:31 +0900

    ストーリーの共通部分を抜き出す

--- src/stories/MyButton.stories.ts
+++ src/stories/MyButton.stories.ts
@@ -6,10 +6,6 @@ type Story = StoryObj<typeof MyButton>;
 const meta: Meta<typeof MyButton> = {
   title: "MyButton",
   component: MyButton,
-};
-
-// 「ボタン」
-export const Default: Story = {
   render: (args) => ({
     components: { MyButton },
     setup() {
@@ -17,34 +13,21 @@ export const Default: Story = {
     },
     template: "<MyButton v-bind='args' />",
   }),
+};
+
+export const Default: Story = {
   args: {
     label: "ボタン",
   },
 };
 
-// 「ログイン」
 export const Login: Story = {
-  render: (args) => ({
-    components: { MyButton },
-    setup() {
-      return { args };
-    },
-    template: "<MyButton v-bind='args' />",
-  }),
   args: {
     label: "ログイン",
   },
 };
 
-// 「会員登録」
 export const SignUp: Story = {
-  render: (args) => ({
-    components: { MyButton },
-    setup() {
-      return { args };
-    },
-    template: "<MyButton v-bind='args' />",
-  }),
   args: {
     label: "会員登録",
   },
