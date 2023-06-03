8c55a37588baab6289e652297c9985c15bcb7627
高橋舞 <tkjnpamw5611@gmail.com>
2023-06-03 15:03:02 +0900

    インストールしたアドオンを利用する為,Storybookの設定ファイルにaddonsフィールドを追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,11 @@ import { StorybookConfig } from "@storybook/vue3-vite";
 const config: StorybookConfig = {
   framework: "@storybook/vue3-vite",
   stories: ["../src/**/*.stories.@(js|ts)"],
-  addons: ["@storybook/addon-controls", "@storybook/addon-actions"],
+  addons: [
+    "@storybook/addon-controls",
+    "@storybook/addon-actions",
+    "@storybook/addon-viewport",
+  ],
 };
 
 export default config;
