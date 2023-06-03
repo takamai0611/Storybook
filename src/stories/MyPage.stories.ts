c481a7d80d49140ddb791507220a701564050095
高橋舞 <tkjnpamw5611@gmail.com>
2023-06-03 15:00:05 +0900

    MyPage コンポーネントのストーリーを作成

---
+++ src/stories/MyPage.stories.ts
@@ -0,0 +1,17 @@
+import MyPage from "../components/MyPage.vue";
+import type { Meta, StoryObj } from "@storybook/vue3";
+
+type Story = StoryObj<typeof MyPage>;
+
+const meta: Meta<typeof MyPage> = {
+  title: "MyPage",
+  component: MyPage,
+  render: () => ({
+    components: { MyPage },
+    template: "<MyPage />",
+  }),
+};
+
+export const Default: Story = {};
+
+export default meta;
